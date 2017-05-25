99155014164083c3edcde007cb9851a8b42922f1
gradyasher <gradyasher@gmail.com>
2017-05-25 16:01:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,28 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models')
 
 const jsonParser = bodyParser.json();
 const app = express();
 
-// log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
+// ShoppingList.create('beans', 2);
+// ShoppingList.create('tomatoes', 3);
+// ShoppingList.create('peppers', 4);
 
-// when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
-});
+// app.get('/shopping-list', (req, res) => {
+//   res.json(ShoppingList.get());
+// });
+
+Recipes.create('breakfast', 'Milk, Cinnamon Toast Crunch')
+Recipes.create('lunch', 'Hot Dog, Bun, Ketchup, Relish')
+Recipes.create('dinner', 'Macaroni, Ground Beef, Hamburger Helper')
+
+app.get('/recipes', (req, res) => {
+	res.json(Recipes.get())
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
